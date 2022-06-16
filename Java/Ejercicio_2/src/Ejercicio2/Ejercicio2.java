6ae21ca64237b91c207f843eec751990934f669e
Ivana Germir <germirivana@gmail.com>
2022-06-15 23:17:09 -0300

    Commit ejercicio 2

---
+++ Java/Ejercicio_2/src/Ejercicio2/Ejercicio2.java
@@ -0,0 +1,13 @@
+
+package Ejercicio2;
+
+//Hacer un programa que calcule e imprima el salario de 
+//un empleado, a partir de sus horas semanales trabajadas
+// y su salario por hora
+public class Ejercicio2 {
+    public static void main(String[] args) {
+         
+        
+    }
+    
+}
