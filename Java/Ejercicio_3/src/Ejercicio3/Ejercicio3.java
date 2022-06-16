2da87add866bb655bc518498e1b97ec9057a6ded
Ivana Germir <germirivana@gmail.com>
2022-06-15 23:18:01 -0300

    commiteamos ejercicio 3

---
+++ Java/Ejercicio_3/src/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,17 @@
+
+package Ejercicio3;
+
+//REctangulo
+//Se solicita calcular el area y el perimetro de un rectangulo
+//para esto deberremos crear las variables alto(int) ancho(int)
+//El usuario debera proporcionar los valores de alto, ancho
+//y despues debe imprimir el resultado en el siguiente formato
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+       
+        System.out.println("Digite el alto del rectangulo");
+        System.out.println("Digite el ancho del rectangulo");
+        
+    }
+}
