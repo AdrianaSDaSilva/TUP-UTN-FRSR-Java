a3d8cd5ba26a7a475c778ce1c3871735471bc96e
Ivana Germir <germirivana@gmail.com>
2022-10-03 01:01:58 -0300

    JavaScript Clase 3 Laboratorio II Tipos de Datos

---
+++ JavaScript/Leccion02/02-01-TiposDeDatos.js
@@ -0,0 +1,17 @@
+// TIPOS DE DATOS JAVASCRIPT
+/*COMENTARIOS DE
+MULTIPLES LINEAS
+*/
+var nombre = 'Ariel'; // dato tipo String
+console.log(nombre); 
+//llamamos a la consola para ejecutar
+
+var numero = 3000; // Tipo numerico
+console.groupCollapsed(numero);
+
+var objeto = {     // Tipo Objeto
+    nombre:'Ariel', 
+    apellido: 'Betancud',
+    telefono: '1234567893'
+}
+console.log(objeto);
