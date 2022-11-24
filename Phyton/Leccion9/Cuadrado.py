f561011c385ea8df707aef52d8c33359c568d642
Ivana Germir <germirivana@gmail.com>
2022-11-24 02:15:03 -0300

    Clase 10 Phyton
    POO Parte 5 Herencia Múltiple

---
+++ Phyton/Leccion9/Cuadrado.py
@@ -0,0 +1,18 @@
+#Hereda de las clses padres, es importante el orden
+
+from FiguraGeometrica import FiguraGeometrica
+from Color import Color
+
+class Cuadrado(FiguraGeometrica, Color):
+    def __init__(self, lado, color):
+        # super.__init__(lado)
+        # en herencia multiple se hace de la siguiente manera
+        FiguraGeometrica.__init__(self, lado, lado)
+        Color.__init__(self, color)
+
+    #Creamos un metodo que va a calcular el area
+    def calcular_area(self):
+        return self.alto * self.ancho
+
+    def __str__(self):
+        return f'{FiguraGeometrica.__str__(self)} {Color.__str__(self)}'
